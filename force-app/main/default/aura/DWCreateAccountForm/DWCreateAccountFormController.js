d021d696a587aaf0257104bbd1eeb3cc0fd6682c
jrattanpal@gmail.com <MyPassword$0>
2017-10-05 14:10:42 -0400

    - Initial sync - Convert project to SFDX from https://github.com/iyeratl/SFDriveWealth

---
+++ force-app/main/default/aura/DWCreateAccountForm/DWCreateAccountFormController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by viyer on 3/2/17.
+ */
+({
+    createAccount: function(component, event, helper) {
+        helper.createAccount(component, helper);
+    },
+    doInit : function(component, event, helper) {
+        helper.getAccountInfo(component, helper);
+        /**/
+    }
+})
