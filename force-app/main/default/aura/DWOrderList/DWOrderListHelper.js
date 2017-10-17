2c83db909ee1d63b33fbd2b03287ca88817eb3bc
jrattanpal@gmail.com <MyPassword$0>
2017-10-17 15:19:29 -0400

    Restyle DW Order List

--- force-app/main/default/aura/DWOrderList/DWOrderListHelper.js
+++ force-app/main/default/aura/DWOrderList/DWOrderListHelper.js
@@ -16,7 +16,7 @@
 
 
                 if($A.util.isUndefined(data.output) || data.output.length <=0){
-                    
+
                     var message = Array();
                     message.push(
                         ["ui:message", {
@@ -32,7 +32,7 @@
 
                     var orders = [], key;
                     for ( key in data.output ) {
-                        data.output[key].Executed_When__c = helper.dateFromSpecialString(data.output[key].Executed_When__c);
+                        data.output[key].Executed_When__c = (data.output[key].Executed_When__c == 'Not Executed')?'':helper.dateFromSpecialString(data.output[key].Executed_When__c);
                         orders.push({value:data.output[key], key:key});
                     }
                     component.find('utils').log('DWOrderStatus.orders: ', orders);
